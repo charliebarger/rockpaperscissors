9685e192e6254e9ef12a498026fc16eaa64f34bf
charliebarger <charliebarger04@gmail.com>
2020-11-09 08:08:33 -0700

    refactored code

--- scripts.js
+++ scripts.js
@@ -1,20 +1,25 @@
+    /* target the computers a and user halves of the playfield and create  Images to be appnded to them */
 
-    
     const compDisplay = document.querySelector('.compPlay');
     const compResult = document.createElement('img');
     
     const userDisplay = document.querySelector('.userPlay');
     const userResult = document.createElement('img');
 
+    /* adjust comp and user scoreboard by appending span elements to them
+    */
     const compScoreBoard = document.querySelector('.two');
     const compScore = document.createElement('span');
     
-
     const userScoreBoard = document.querySelector('.one');
     const userScore = document.createElement('span');
     
+    /*create a place for the status of each round to be displayed */
+
     const roundAnnouncement = document.querySelector('.roundAnnouncement');
     const announceRound = document.createElement('p');
+
+    // create full page overlay 
 
     const body = document.querySelector('body');
     const overlay = document.createElement('div');
@@ -23,7 +28,7 @@
     overlay.classList.add("divvy");
     const playAgain = document.createElement('button')
 
-
+    // initial 0 that is displayed on the screen when it loads
     const startNum = document.querySelectorAll('.startnum');
     
     const buttons = document.querySelectorAll('button');
@@ -31,25 +36,36 @@
     let win = 0;
     let loss = 0;
     
+    //on each button click remove initial 0s and then call functions
+
     buttons.forEach((button) => {
         button.addEventListener('click', function() {
             startNum.forEach((num) => {
             num.remove();
             })
             let robotPick = generateComputerChoice();
-            if (button.className == 'Rock'){
-                userResult.src = "./Images/Rock_right.svg"
-            }
-            else if (button.className == 'Paper'){
-                userResult.src = "./Images/Paper_right.svg"
-            }
-            else {
-                userResult.src = "./Images/Scissors_right.svg"
-            }
-            userDisplay.appendChild(userResult);
+            displayUserChoice(button.className);
             game(button.className, robotPick);
         });
     });
+
+    //append SVG to user play field depnding on what is selected
+     function displayUserChoice(choice){
+            if (choice == 'Rock'){
+                userResult.src = "./Images/Rock_right.svg";
+                userResult.alt = "Rock";
+            }
+            else if (choice == 'Paper'){
+                userResult.src = "./Images/Paper_right.svg";
+                userResult.alt = "Paper";
+            }
+            else {
+                userResult.src = "./Images/Scissors_right.svg";
+                userResult.alt = "Scissors";
+            }
+            userDisplay.appendChild(userResult);
+    }
+    /*pick a random choice, append the coresponding svg to playfield and return the selected choice */
 
         function  generateComputerChoice() {
             const rps = ['Rock', 'Paper', 'Scissors'];
@@ -57,19 +73,24 @@
             choice = rps[random];
             if (choice == 'Rock'){
                 compResult.src = './Images/Rock_left.svg';
+                compResult.alt = 'Rock';
             }
             else if (choice == 'Paper') {
                 compResult.src = './Images/Paper_left.svg';
+                compResult.alt = "Paper";
             }
             else {
                 compResult.src = './Images/Scissors_left.svg'
+                compResult.alt = "Scissors";
             }
             compDisplay.appendChild(compResult);
             return(choice);
         }
+    
+    //decide who won and return an announcement 
 
         function decideWinner(userChoice, computerChoice) {
-            let winnerAnnouncement = (`You Win! ${userChoice} beats ${computerChoice}!`);1
+            let winnerAnnouncement = (`You Win! ${userChoice} beats ${computerChoice}!`);
             let loserAnnouncement = (`You Loose ${computerChoice} beats ${userChoice}!`);
             
             switch (userChoice) {
@@ -99,34 +120,37 @@
                     else {
                         return (winnerAnnouncement);
                     }
-                    
-                
             }
         }
 
+        /* */
+    
         function game(choice, computerChoice) {
             let roundWinner = decideWinner(choice, computerChoice);
 
-                if (roundWinner.search('Win') > -1){
+            if (roundWinner.search('Win') > -1){
                     win++;
                 }
-                else if (roundWinner.search('Loose') > -1) {
+            else if (roundWinner.search('Loose') > -1) {
                     loss++;    
                     }
-                announceRound.textContent = roundWinner;
-                roundAnnouncement.appendChild(announceRound)
-                userScore.textContent = win;
-                userScoreBoard.appendChild(userScore);
+            announceRound.textContent = roundWinner;
+            roundAnnouncement.appendChild(announceRound);
+
+            userScore.textContent = win;
+            userScoreBoard.appendChild(userScore);
                 
-                compScore.textContent = loss;
-                compScoreBoard.appendChild(compScore)
-               
-                if (win == 3){
-                    restart('win')
+            compScore.textContent = loss;
+            compScoreBoard.appendChild(compScore)
+            
+            if (win == 3){
+                restart('win')
                     }
-                else if (loss == 3){
-                    restart('loose')
-                } }
+            else if (loss == 3){
+                restart('loose')
+            } }
+
+        /*append overlay with a play again button that restors the game to its original settings and then removes the overlay*/
 
         function restart(result){
             body.appendChild(overlay);
@@ -140,12 +164,14 @@
             playAgain.textContent = 'Play Again?'
             overlay.appendChild(playAgain)
             playAgain.addEventListener('click', function() {
-            win = 0;
-            loss = 0;
-            compScore.textContent = loss, userScore.textContent = win;
-            compScoreBoard.appendChild(compScore);
-            userScoreBoard.appendChild(userScore)
-            overlay.remove();
-            announceRound.remove();
+                win = 0;
+                loss = 0;
+                compScore.textContent = loss, userScore.textContent = win;
+                compScoreBoard.appendChild(compScore);
+                userScoreBoard.appendChild(userScore)
+                overlay.remove();
+                announceRound.remove();
+                userResult.remove();
+                compResult.remove();
             })
         }
